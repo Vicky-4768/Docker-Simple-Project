b7127c99d7e0a0567d0d4c41baaa52662e09fd19
Vicky-4768 <fpro12068@gmail.com>
2025-01-13 12:12:25 +0530

    app.py update for run in container continously

--- weather_app/app.py
+++ weather_app/app.py
@@ -1,32 +1,52 @@
+import time
 import requests
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, exc
 from sqlalchemy.orm import sessionmaker
 from database.models import Base, Weather
 
-API_KEY = 'your-api-key'
+API_KEY = 'API-KEY'
 CITY = 'Indore'
 URL = f'http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}'
 
 def fetch_weather_data():
-    response = requests.get(URL)
-    data = response.json()
-    return data
+    try:
+        response = requests.get(URL)
+        response.raise_for_status()  # Raise an error for bad status codes
+        data = response.json()
+        return data
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data from OpenWeather API: {e}")
+        return None
 
 def main():
-    engine = create_engine('mysql+mysqlconnector://your-sql-user:your-sql-passwd@your-database-IP or localhost/weather_db')
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    
-    weather_data = fetch_weather_data()
-    weather = Weather(
-        city=weather_data['name'],
-        temperature=weather_data['main']['temp'],
-        description=weather_data['weather'][0]['description']
-    )
-    
-    session.add(weather)
-    session.commit()
+    try:
+        engine = create_engine('mysql+mysqlconnector://db_user:db_passwd@localhost or IP/weather_db')
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        while True:
+            weather_data = fetch_weather_data()
+            if weather_data:
+                # Convert temperature from Kelvin to Celsius
+                temperature_celsius = weather_data['main']['temp'] - 273.15 
+                weather = Weather(
+                    city=weather_data['name'],
+                    temperature=temperature_celsius,
+                    description=weather_data['weather'][0]['description']
+                )
+
+                session.add(weather)
+                session.commit()
+                print(f"Weather data for {CITY} stored in database: {temperature_celsius}°C")
+            else:
+                print("Failed to fetch weather data.")
+
+            # Wait for 10 minutes before fetching data again
+            time.sleep(600)
+
+    except exc.SQLAlchemyError as e:
+        print(f"Database error: {e}")
 
 if __name__ == "__main__":
     main()
